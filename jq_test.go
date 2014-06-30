c22bca936902d75fbf12115d009310f178733617
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-06-30 04:22:46 -0700

    JQStack test

--- jq_test.go
+++ jq_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type MockFilter []string
 
@@ -20,5 +23,41 @@ func TestJoinFilter(t *testing.T) {
 	filter = JoinFilter(MockFilter{"!"})
 	if filter != "!" {
 		t.Fatalf("incorrect one-value filter: %v", filter)
+	}
+}
+
+func TestJQStack(t *testing.T) {
+	s := new(JQStack)
+
+	s.Push(MockFilter{"hello"})
+	s.Push(MockFilter(nil))
+	s.Push(MockFilter{"world"})
+	fs := s.JQFilter()
+	if !reflect.DeepEqual(fs, []string{"hello", "world"}) {
+		t.Fatalf("incorrect filter stack: %v", fs)
+	}
+
+	s.Pop()
+	fs = s.JQFilter()
+	if !reflect.DeepEqual(fs, []string{"hello"}) {
+		t.Fatalf("incorrect filter stack: %v", fs)
+	}
+
+	s.Pop()
+	fs = s.JQFilter()
+	if !reflect.DeepEqual(fs, []string{"hello"}) {
+		t.Fatalf("incorrect filter stack: %v", fs)
+	}
+
+	s.Pop()
+	fs = s.JQFilter()
+	if len(fs) != 0 {
+		t.Fatalf("incorrect filter stack: %v", fs)
+	}
+
+	s.Pop()
+	fs = s.JQFilter()
+	if len(fs) != 0 {
+		t.Fatalf("incorrect filter stack: %v", fs)
 	}
 }
