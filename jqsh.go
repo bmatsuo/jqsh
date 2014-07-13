ee7f88e98ce463beacc85eb84b36856cfa0692b5
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-07-13 01:20:46 -0700

    godoc preformatting fix

--- jqsh.go
+++ jqsh.go
@@ -51,12 +51,12 @@ to the jqsh filter stack. Filters can be removed from the filter stack with ":po
 	> :pop
 
 
-	:<cmd> <arg1> <arg2> ..			execute cmd with the given arguments
-	:<cmd> ... +<argN>				execute cmd with an argument containing spaces (argN)
-	.								shorthand for ":write"
-	..								shorthand for ":pop"
-	?<filter>						shorthand for ":peek <filter>"
-	<filter>						shorthand for ":push <filter>"
+	:<cmd> <arg1> <arg2> ..         execute cmd with the given arguments
+	:<cmd> ... +<argN>              execute cmd with an argument containing spaces (argN)
+	.                               shorthand for ":write"
+	..                              shorthand for ":pop"
+	?<filter>                       shorthand for ":peek <filter>"
+	<filter>                        shorthand for ":push <filter>"
 
 Note that "." is a valid jq filter but in jqsh it lacks meaning in the filter
 stack.  So instead of intepreting it as a filter it is a shorthand for
