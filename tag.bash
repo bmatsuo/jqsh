ad413273891b68c0792ef5f7ecc20fd8d0404a4c
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-07-14 02:08:13 -0700

    generate version.go with each execution of tag.bash

--- tag.bash
+++ tag.bash
@@ -2,10 +2,30 @@
 
 TAG_NAME="$1"
 
+# NEXT_NAME is the next version number to use.  The name is a little misleading
+# because it will probably have a suffix like "dev".  And there will never be a
+# tag with that suffix.
+NEXT_NAME="$2"
+
 if [ -z "$TAG_NAME" ]; then
     echo 1>&2 'no tag name given'
     exit 1
 fi
+
+if [ -z "$NEXT_NAME" ]; then
+    echo 1>&2 'no next tag given'
+    exit 1
+fi
+
+version_go() {
+VERSION_GO=<<EOGO
+// WARNING: this file is generated do not modify it manually.
+package main
+
+const Version = "$1"
+EOGO
+echo "$VERSION_GO"
+}
 
 build() {
     os=$1
@@ -27,6 +47,10 @@ if [ -n "`ls *.tar.gz 2>/dev/null`" ]; then
     exit 1
 fi
 
+version_go "$TAG_NAME" > version.go
+git add version.go
+git commit -m "bump version to $TAG_NAME"
+
 git tag -a "$1"
 
 build darwin amd64
@@ -34,3 +58,6 @@ build linux amd64
 md5 jqsh"${TAG_NAME}".*.tar.gz > jqsh"${TAG_NAME}".md5
 
 echo "all distributions built successfully"
+
+version_go "$NEXT_NAME" > version.go
+git commit -m "bump version to $NEXT_NAME"
